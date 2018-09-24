26795d31bfba47b5e550d88566502b74c2dc48a7
Jihui Zhu <zhujihui@gmail.com>
2018-09-24 17:59:07 +0800

    some change

--- movie_watched/main.py
+++ movie_watched/main.py
@@ -1,13 +1,7 @@
 import http.client
-import re
-from urllib import parse
 import json
 import csv
-
-'''
-4. 替换userid
-5. writerow 第一行
-'''
+from urllib import parse
 
 domain = "frodo.douban.com"
 user_agent = "com.douban.frodo"
@@ -18,40 +12,54 @@ headers = {
 }
 
 
-def get_access_token():
-    payload = "client_id=0ab215a8b1977939201640fa14c66bab&client_secret=22b2cf86ccc81009&grant_type=password&username=zhujihui1991%40gmail.com&password=Zhujihui6578602%25"
-    conn_url.request("POST", "/service/auth2/token", payload, headers)
-    res = conn_url.getresponse().read().decode()
-    access_token = re.search('[0-9a-f]{32}', res).group()
-    # user_id = re.search()
-    return str(access_token)
+def log_in(username, password):
+    # 获取登录后的access_token和user_id
+    api_url = "/service/auth2/token"
+    request_body = {
+        "client_id": "0ab215a8b1977939201640fa14c66bab",
+        "client_secret": "22b2cf86ccc81009",
+        "grant_type": "password",
+        "username": username,
+        "password": password,
+
+    }
+    conn_url.request("POST", api_url, parse.urlencode(request_body), headers)
+    response_body = conn_url.getresponse().read().decode("utf-8")
+    access_token_dict = json.loads(response_body)
+    access_token_str = access_token_dict['access_token']
+    user_id_no = access_token_dict['douban_user_id']
+    return access_token_str, user_id_no
 
 
-def get_all_movies(count, start):
-    headers['Authorization'] = 'Bearer ' + get_access_token()
+def get_all_movies(access_token_str, user_id_no, count, start):
+    # 获取所有登录用户的标记过的电影
+    headers['Authorization'] = 'Bearer ' + access_token_str
     body = {'count': count,  # 一次返回多少个数据
             'start': start,
             'status': 'done',
             'type': 'movie',
             }
-    request_url = '/api/v2/user/51530953/interests?' + parse.urlencode(body)  # 请求参数加密并拼接成url
+    request_url = '/api/v2/user/' + user_id_no + '/interests?' + parse.urlencode(body)  # 请求参数加密并拼接成url
     conn_url.request("GET", request_url, headers=headers)
-    response_data = conn_url.getresponse().read() # read结果是bytes型
+    response_data = conn_url.getresponse().read()  # read结果是bytes型
     dict_main = json.loads(response_data.decode("utf-8"))  # 先将bytes转为str，再将str转为dict
     total_amount = dict_main['total']
     return dict_main, total_amount
 
 
 def write_into_csv(count, start):
+    # 把获取的标记过的电影写入CSV文件
     csv_file = open("csvData.csv", "a+", newline='', encoding='utf-8')  # a+追加并可读可写
     writer = csv.writer(csv_file)
     # writer.writerow(['电影名称', '豆瓣评分', '我的评分', '我的评语', '评分时间'])
-    all_movies = get_all_movies(count, start)
+    all_movies = get_all_movies(access_token, user_id, count, start)
     total_amount = all_movies[1]  # 看过的电影总数
-    inn = total_amount%count
-    for i in range(1, inn):
-        start = (i-1)*50 + 1
-        marked_movie_list = get_all_movies(count, start)[0]['interest']  # 看过的所有电影列表
+    times = total_amount % count
+    print(times)
+    for i in range(1, times):
+        # start = (i-1)*50 + 1
+        start = 1500
+        marked_movie_list = get_all_movies(access_token, user_id, count, start)[0]['interests']  # 看过的所有电影列表
         # number = 0
         for marked_movie in marked_movie_list:  # 从看过的所有电影列表中循环并格式化输出到csv文件
             comment = marked_movie['comment']
@@ -65,6 +73,9 @@ def write_into_csv(count, start):
 
 
 if __name__ == "__main__":
-    print(get_all_movies(50, 1))
+    user_name = "zhujihui1991@gmail.com"
+    pass_word = "Zhujihui6578602%"
+    log_in_return = log_in(user_name, pass_word)
+    access_token = log_in_return[0]
+    user_id = log_in_return[1]
     write_into_csv(50, 1)
-
